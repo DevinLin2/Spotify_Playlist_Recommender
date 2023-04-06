0c136590b94749950d00fb0982fb3d98f79d451f
Devin Lin <dxl702@case.edu>
2023-04-06 00:41:16 -0400

    hard coded some results for demo purposes but this will be the model for our results when we get that working

--- playlist_recommender/src/pages/index.js
+++ playlist_recommender/src/pages/index.js
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
-import {useSession, signIn, signOut} from 'next-auth/react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Head from 'next/head'
 
 export default function Home() {
 
   const [userInput, setUserInput] = useState("");
-  const {data: session} = useSession();
+  const { data: session } = useSession();
+  const [showResult, setShowResult] = useState(false);
 
-  function handleSubmit() {
+  function handleSubmit(event) {
+    event.preventDefault();
     console.log(userInput);
+    setShowResult(true);
   }
 
   return (
@@ -33,7 +37,8 @@ export default function Home() {
           {!session && <Button onClick={() => signIn()}>Sign In</Button>}
         </Container>
       </Navbar>
-      <Form>
+      <br></br>
+      <Form onSubmit={handleSubmit}>
         <Container>
           <Row>
             <Col>
@@ -45,9 +50,43 @@ export default function Home() {
           </Row>
           <Row>
             <Col>
-              <Button className="float-end" variant="primary" onClick={handleSubmit}>Get Recommendations</Button>
+              <Button className="float-end" variant="primary" type="submit">Get Playlists</Button>
             </Col>
           </Row>
+          {showResult && <div>
+            <Row>
+              <Col>
+                Results:
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="https://i.scdn.co/image/ab67706c0000bebbc8dfff3cf472a74090fd14fc" />
+                  <Card.Body>
+                    <Card.Title>Lofi Hop Hop Radio 2023</Card.Title>
+                    <Card.Text>
+                      List of first few songs
+                    </Card.Text>
+                    <Button variant="primary" href="https://open.spotify.com/playlist/05OkqemhVmD27zXfdnyNsy">Go to Playlist</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <br></br>
+            <Row>
+              <Col>
+                How were our results?
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6}>
+                <Button variant="success">Excellent</Button>{' '}
+                <Button variant="warning">Mediocre</Button>{' '}
+                <Button variant="danger">Terrible</Button>{' '}
+              </Col>
+            </Row>
+          </div>}
         </Container>
       </Form>
     </div>
